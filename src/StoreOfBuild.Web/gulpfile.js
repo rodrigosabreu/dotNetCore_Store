d66df78be8247e33260e498f0a190584b00aedc9
rodrigosabreu <rodrigo.s.abreu@gmail.com>
2018-03-15 10:51:18 -0300

    ajustes manuais no css e js

--- src/StoreOfBuild.Web/gulpfile.js
+++ src/StoreOfBuild.Web/gulpfile.js
@@ -21,7 +21,7 @@ gulp.task('js', function(){
        './node_modules/jquery-ajax-unobtrusive/jquery.unobtrusive-ajax.min.js',
        './node_modules/jquery/dist/jquery.min.js',
        './node_modules/jquery-validation/dist/jquery.validate.min.js',
-       './node_modules/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.js',         
+       './node_modules/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.min.js',         
        './js/**/*.js',
        ])
        .pipe(gulp.dest('wwwroot/js/'))
